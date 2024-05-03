fe9a8c9d1369a1990cd0016c9079fea1e8a995d1
IanFerranti <86329078+IanFerranti@users.noreply.github.com>
2024-05-03 15:25:43 -0300

    Completo

---
+++ lab3/src/ServidorImpl.java
@@ -0,0 +1,97 @@
+/**
+ * Laboratorio 3  
+ * Autor: Lucio Agostinho Rocha
+ * Ultima atualizacao: 04/04/2023
+ */
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ServidorImpl implements IMensagem{
+	
+	private Principal p;
+    
+    public ServidorImpl() {
+    	this.p = new Principal();
+    }
+    
+    //Cliente: invoca o metodo remoto 'enviar'
+    //Servidor: invoca o metodo local 'enviar'
+    @Override
+    public Mensagem enviar(Mensagem mensagem) throws RemoteException {
+        Mensagem resposta;
+        try {
+        	System.out.println("Mensagem recebida: " + mensagem.getMensagem());
+			resposta = new Mensagem(parserJSON(mensagem.getMensagem()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			resposta = new Mensagem("{\n" + "\"result\": false\n" + "}");
+		}
+        return resposta;
+    }    
+    
+    public String parserJSON(String json) throws Exception {
+    	json = json.substring(1, json.length() - 1);
+	    String[] campos = json.split(",");
+	    String method = "";
+	    String args = "";
+	    String saida = "";
+
+	    for (String campo : campos) {
+	        String[] partes = campo.split(":");
+	        String chave = partes[0].trim().replace("\"", "");
+	        String valor = partes[1].trim().replace("\"", "");
+	        
+	        
+	        if (chave.equals("method")) {
+	            method = valor;
+	        } else if (chave.equals("args")) {
+	            args = valor;
+	        }
+	    }
+
+
+	    // Verificar se method é nulo antes de acessá-lo
+	    if(method != null) {
+	        //executar o comando
+	        if(method.equals("read")) {
+	            String fortuna = p.read();
+	            saida = "{\n"
+                        + "\"result\": \"" + fortuna+"\"\n}";
+	        } else if(method.equals("write")) {
+	            if(!args.isEmpty()) {
+	            	args = args.replace("[", "").replace("]", "");
+	                p.write(args);
+	                saida = "{\n"
+	                        + "\"result\": \"" + args + "\""
+	                        + "\n}";
+	            } else {
+	                saida = "{"
+	                        + "\"result\": \"false\""
+	                        + "}";
+	            }
+	        }
+	    }
+	    return saida;
+    }
+	
+    public void iniciar(){
+
+    try {
+            Registry servidorRegistro = LocateRegistry.createRegistry(1099);            
+            IMensagem skeleton  = (IMensagem) UnicastRemoteObject.exportObject(this, 0); //0: sistema operacional indica a porta (porta anonima)
+            servidorRegistro.rebind("servidorFortunes", skeleton);
+            System.out.print("Servidor RMI: Aguardando conexoes...");
+                        
+        } catch(Exception e) {
+            e.printStackTrace();
+        }        
+
+    }
+    
+    public static void main(String[] args) {
+        ServidorImpl servidor = new ServidorImpl();
+        servidor.iniciar();
+    }    
+}
